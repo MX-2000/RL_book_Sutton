ae6ca0481d6e3f8d1263b567540c6eb98d3a1bb9
Max <max.aguirre2424b@gmail.com>
2024-10-26 11:44:08 +0200

    update seeding to match gymnasium

--- envs/bandit.py
+++ envs/bandit.py
@@ -24,7 +24,7 @@ class KBanditEnv(gym.Env):
             for i in range(k):
 
                 # We generate the true value according to normal distribution with mean 0 and unit variance
-                true_q = np.random.normal(loc=0, scale=1)
+                true_q = self.np_random.normal(loc=0, scale=1)
                 true_q_values[i] = true_q
         else:
             START_Q = 0
@@ -45,13 +45,13 @@ class KBanditEnv(gym.Env):
         # Continuous task
         terminated = False
         truncated = False
-        reward = np.random.normal(loc=self.true_q_values[action], scale=1)
+        reward = self.np_random.normal(loc=self.true_q_values[action], scale=1)
         obs = np.array([0.0], dtype=np.float32)
 
         if not self.stationnary:
             # We update all the q*(a) by a independant random walk
             for i in range(self.k):
-                increment = np.random.normal(loc=0.0, scale=0.01)
+                increment = self.np_random.normal(loc=0.0, scale=0.01)
                 self.true_q_values[i] += increment
 
         return obs, reward, terminated, truncated, self._get_info()
